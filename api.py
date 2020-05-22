f82bd604a45e8f78a5e9654f82f6b2ab7d9042b4
avn-0216-m <maidenofthebirdeternal@gmail.com>
2020-05-22 19:19:57 +0100

    init

---
+++ api.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+import flask
+from flask import request
+import random
+import os
+import sys
+import base64
+
+healthcheck_messages = ["hiya!", "hey there!", "beep boop!", "i'm alive. <3"]
+
+app = flask.Flask("avn-0216")
+
+@app.route('/', methods=['GET'])
+def healthcheck():
+
+    return random.choice(healthcheck_messages)
+
+@app.route('/', methods=['POST'])
+def interface():
+
+    reply = "beep boop!"
+
+    if "intercept" in os.listdir():
+        print("Intercepting transmission.")
+        with open("intercept_req_writing", "w") as req_file:
+            for header_key, header_value in request.headers:
+                req_file.write(header_key + ": " + header_value + "\n")
+            req_file.write(str(request.data))
+            req_file.close()
+        os.rename("intercept_req_writing", "intercept_req")
+
+        while "intercept_resp" not in os.listdir():
+            pass
+        with open("intercept_resp") as resp_file:
+            reply = resp_file.readline()
+            resp_file.close()
+            os.remove('intercept_resp')
+
+    else:
+        print("No interception.")
+
+        post_body = str(request.data).lower()
+
+        if "beep" in post_body:
+            reply = "boop!"
+        elif "boop" in post_body:
+            reply = "beep!"
+        elif any(lovely_word in post_body for lovely_word in ["ily", "i love you"]):
+            reply = "i love you too!! <3"
+        elif "<3" in post_body:
+            reply = "<3 <3 <3"
+        elif "ping" in post_body:
+            reply = "pong~!"
+
+    return reply
+
+app.run(port=216)
---
+++ intercept.py
@@ -0,0 +1,24 @@
+import os
+from datetime import datetime
+
+open('intercept', 'a').close()
+try:
+    while True:
+        if "intercept_req" in os.listdir():
+            os.system("cls")
+            print("Message intercepted at " + str(datetime.now()))
+            print("-------------------------------------------")
+            with open('intercept_req') as req_file:
+                for line in req_file.readlines():
+                    print(line)
+            req_file.close()
+            os.remove('intercept_req')
+            reply = input("> ")
+            with open('intercept_resp_writing', 'w') as resp_file:
+                resp_file.write(reply)
+                resp_file.close()
+            os.rename('intercept_resp_writing', 'intercept_resp')
+            print("Response sent.")
+except KeyboardInterrupt:
+    print("Cleanly shutting down.")
+    os.remove('intercept')
